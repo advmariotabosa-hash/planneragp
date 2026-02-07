8c6881bcb0accc967194d597137f9d6bc32bb5eb
advmariotabosa-hash <advmariotabosa@gmail.com>
2026-02-07 10:06:49 -0300

    Update atualizar-reuniao.dto.ts

--- src/reunioes/dto/atualizar-reuniao.dto.ts
+++ src/reunioes/dto/atualizar-reuniao.dto.ts
@@ -1,1 +1,49 @@
+import { IsString, IsOptional, IsDateString, IsEnum } from 'class-validator';
 
+enum TipoReuniao {
+  ORDINARIA = 'ORDINARIA',
+  EXTRAORDINARIA = 'EXTRAORDINARIA',
+  INAUGURAL = 'INAUGURAL',
+  OUTRO = 'OUTRO'
+}
+
+enum StatusReuniao {
+  AGENDADA = 'AGENDADA',
+  EM_ANDAMENTO = 'EM_ANDAMENTO',
+  CONCLUIDA = 'CONCLUIDA',
+  CANCELADA = 'CANCELADA'
+}
+
+export class AtualizarReuniaoDto {
+  @IsString()
+  @IsOptional()
+  titulo?: string;
+
+  @IsString()
+  @IsOptional()
+  descricao?: string;
+
+  @IsEnum(TipoReuniao)
+  @IsOptional()
+  tipo?: TipoReuniao;
+
+  @IsDateString()
+  @IsOptional()
+  dt_inicio?: string;
+
+  @IsDateString()
+  @IsOptional()
+  dt_fim?: string;
+
+  @IsString()
+  @IsOptional()
+  localizacao?: string;
+
+  @IsString()
+  @IsOptional()
+  link_videochamada?: string;
+
+  @IsEnum(StatusReuniao)
+  @IsOptional()
+  status?: StatusReuniao;
+}
