c8b8c068b2a760c06bc92c8fa00654ea4e184683
advmariotabosa-hash <advmariotabosa@gmail.com>
2026-02-07 19:11:16 -0300

    Create atualizar-pendencia.dto.ts
    adiciona DTO para atualizar pendência

---
+++ src/pendencias/dto/atualizar-pendencia.dto.ts
@@ -0,0 +1,33 @@
+import { IsString, IsOptional, IsDateString, IsEnum } from 'class-validator';
+
+export enum StatusPendencia {
+  PENDENTE = 'pendente',
+  CONCLUIDA = 'concluida'
+}
+
+export class AtualizarPendenciaDto {
+  @IsString()
+  @IsOptional()
+  titulo?: string;
+
+  @IsString()
+  @IsOptional()
+  responsavel_id?: string;
+
+  @IsDateString()
+  @IsOptional()
+  prazo?: string;
+
+  @IsEnum(StatusPendencia)
+  @IsOptional()
+  status?: StatusPendencia;
+
+  @IsString()
+  @IsOptional()
+  descricao?: string;
+}
+```
+
+### **4. Commit:**
+```
+feat: adiciona DTO para atualizar pendência
