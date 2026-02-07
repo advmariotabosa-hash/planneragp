47b1d905d28fe3034c889fb6a2838c4779198ac0
advmariotabosa-hash <advmariotabosa@gmail.com>
2026-02-07 19:18:44 -0300

    Create pendencias.module.ts
    module de pendências

---
+++ src/pendencias/pendencias.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PendenciasController } from './pendencias.controller';
+import { PendenciasService } from './pendencias.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [PendenciasController],
+  providers: [PendenciasService],
+  exports: [PendenciasService],
+})
+export class PendenciasModule {}
+```
