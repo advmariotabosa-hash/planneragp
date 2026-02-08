69cdd755c294e43622034a0cce73401b6f149e5c
advmariotabosa-hash <advmariotabosa@gmail.com>
2026-02-07 21:06:32 -0300

    Create pautas.module.ts

---
+++ src/pautas/pautas.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { PautasController } from './pautas.controller';
+import { PautasService } from './pautas.service';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({
+  imports: [PrismaModule],
+  controllers: [PautasController],
+  providers: [PautasService],
+  exports: [PautasService],
+})
+export class PautasModule {}
